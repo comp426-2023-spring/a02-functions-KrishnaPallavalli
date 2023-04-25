ca0cc5ac22ef8f6311006aad43f02c58c852522b
KrishnaPallavalli <32885657+KrishnaPallavalli@users.noreply.github.com>
2023-04-25 13:12:56 -0400

    Finished up testing the output.

--- cli.js
+++ cli.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-
 import minimist from 'minimist';
 import fetch from 'node-fetch';
 import moment from 'moment-timezone';
@@ -9,13 +8,15 @@ const args = minimist(process.argv.slice(2));
 const timezone = moment.tz.guess();
 
 if (args.h) {
-  console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+  console.log(`
+Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
     -h            Show this help message and exit.
     -n, -s        Latitude: N positive; S negative.
     -e, -w        Longitude: E positive; W negative.
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.`);
+    -j            Echo pretty JSON from open-meteo API and exit.
+  `);
   process.exit(0);
 }
 
@@ -24,33 +25,23 @@ const longitude = (args.e || args.w) * (args.e ? 1 : -1);
 const timeZone = args.z || timezone;
 const days = args.d || 1;
 const jsonOutput = args.j;
-
 const requestUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&timezone=${timeZone}`;
 
 async function getWeatherData() {
   try {
     const response = await fetch(requestUrl);
     const data = await response.json();
-
     if (jsonOutput) {
       console.log(JSON.stringify(data, null, 2));
       return;
     }
 
-    const precipHours = data.daily.precipitation_hours[days];
-
     if (days === 0) {
-      console.log(`Today: `);
+      console.log('today');
     } else if (days === 1) {
-      console.log(`Tomorrow: `);
+      console.log('tomorrow');
     } else {
-      console.log(`In ${days} days: `);
-    }
-
-    if (precipHours === 0) {
-      console.log('No precipitation expected.');
-    } else {
-      console.log(`${precipHours} hours of precipitation expected.`);
+      console.log(`in ${days} days`);
     }
   } catch (error) {
     console.error('Error:', error.message);
